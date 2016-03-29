2dcc7d74c4bacf12a13a9896f857b9d2837f3420
Wojciech Olejnik <divoolej@gmail.com>
2016-03-29 22:44:42 +0200

    Add Visit model

---
+++ api/models/Visit.js
@@ -0,0 +1,13 @@
+module.exports = {
+  attributes: {
+    ip_address: {
+      type: 'string',
+      required: true,
+    },
+
+    timestamp: {
+      type: 'datetime',
+      required: true,
+    }
+  }
+}
