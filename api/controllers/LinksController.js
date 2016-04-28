95242ddf8eb0b1ab8e6af85945a02f929a25bf35
Wojciech Olejnik <divoolej@gmail.com>
2016-04-28 07:48:35 +0200

    Generate link api

--- api/controllers/LinksController.js
+++ api/controllers/LinksController.js
@@ -1,7 +1,7 @@
 /**
- * UserController
+ * LinksController
  *
- * @description :: Server-side logic for managing users
+ * @description :: Server-side logic for managing links
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
