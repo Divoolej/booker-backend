55c77285bd10fddf3649236dcd81f58a93b87293
Wojciech Olejnik <divoolej@gmail.com>
2016-05-10 21:08:56 +0200

    Add correct config for Postgres on Heroku
    TG-44 #in-progress

--- config/connections.js
+++ config/connections.js
@@ -84,10 +84,12 @@ module.exports.connections = {
 
   productionPostgresConnection: {
     adapter: 'sails-postgresql',
-    host: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: true
   },
 
   /***************************************************************************
