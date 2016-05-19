0c45dc2c80dfa3e37f1434390d0f423bccc0f482
Wojciech Olejnik <divoolej@gmail.com>
2016-05-19 06:19:20 +0200

    Add the #index action to CategoriesController
    TG-43 #in-progress

--- api/controllers/CategoriesController.js
+++ api/controllers/CategoriesController.js
@@ -7,5 +7,20 @@
 
 module.exports = {
 	
+	index: function(req, res) {
+    var query = {};
+    if (req.query.userId) { query.userId = req.query.userId; }
+    if (req.query.id) { query.id = req.query.id; }
+    if (req.query.name) { query.name = req.query.name; }
+    if (req.query.limit) { query.limit = req.query.limit; }
+    Category.find(query).exec(function(error, categories) {
+      if (error) {
+        return res.json(error);
+      } else {
+        return res.json({ categories: categories });
+      }
+    });
+  },
+
 };
 
