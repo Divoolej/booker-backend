9e20ca70a262bef29e0203afaaeb288ab3bac941
Wojciech Olejnik <divoolej@gmail.com>
2016-05-19 06:21:49 +0200

    Add the #index action to LinksController
    TG-28 #in-progress

--- api/controllers/LinksController.js
+++ api/controllers/LinksController.js
@@ -6,6 +6,19 @@
  */
 
 module.exports = {
-	
-};
+  index: function(req, res) {
+    var query = {};
+    if (req.query.userId) { query.userId = req.query.userId; }
+    if (req.query.id) { query.id = req.query.id; }
+    if (req.query.limit) { query.limit = req.query.limit; }
+    Link.find(query).exec(function(error, links) {
+      if (error) {
+        return res.json(error);
+      } else {
+        return res.json({ links: links });
+      }
+    });
+  },
 
+
+};
